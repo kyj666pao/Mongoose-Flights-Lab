07f28ca320b94a0c30fc4cf3a8a8e61f19057901
Jerry Pao <kyjerry0424@gmail.com>
2023-04-20 12:19:49 -0400

    add link for flight on main page

--- config/database.js
+++ config/database.js
@@ -3,18 +3,11 @@ import mongoose from "mongoose";
 // shortcut to mongoose.connection object
 const db = mongoose.connection
 
-mongoose
-  .connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+mongoose.connect(process.env.DATABASE_URL)
 
+db.on('connected', function() {
+  console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`)
+})
 
 
 
